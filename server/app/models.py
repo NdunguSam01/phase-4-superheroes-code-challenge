c100675161f9952d72b08ea35db98c033ba9c6f7
Samuel Muigai <ndungusam01@gmail.com>
2024-01-28 10:36:43 +0300

    Models created

---
+++ server/app/models.py
@@ -0,0 +1,53 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False, unique=True)
+    super_name=db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #Secondary indicates the association table that connects the Hero and Power tables
+    powers=db.relationship("Power", secondary= "heropowers", back_populates="heroes")
+
+    def __repr__(self):
+        return f"Superhero name: {self.name}\nSuper name: {self.super_name}\nCreated at: {self.created_at}"
+
+class Power(db.Model, SerializerMixin):
+
+    __tablename__= "powers"
+
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String, nullable=False, unique=True)
+    description=db.Column(db.String, nullable=False) 
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #Secondary indicates the association table that connects the Hero and Power tables
+    heroes=db.relationship("Heroes", secondary= "heropowers", back_populates="powers")
+
+    def __repr__(self):
+        return f"Power name: {self.name}\nDescription: {self.description}\nCreated at: {self.created_at}"
+
+class HeroPower(db.Model, SerializerMixin):
+
+    __tablename__= "heropowers"
+
+    id=db.Column(db.Integer, primary_key=True)
+    strength=db.Column(db.String, nullable=False)
+    hero_id=db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id=db.Column(db.Integer, db.ForeignKey('powers.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    #Creating attributes in the heropowers table that will reference the Hero and Powers tables
+    hero=db.relationship("Hero", back_populates="powers")
+    power=db.relationship("Power",back_populates="heroes")
+
+    def __repr__(self):
+        return f"Strength: {self.strength}\nSuper Hero: {self.hero}\nPower name: {self.power}"
